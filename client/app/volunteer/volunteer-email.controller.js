e43162251827d0667239ff735701930d725ff5a5
devshawn <musicalshawn@gmail.com>
2016-04-29 13:04:51 -0500

    Fixing email

--- client/app/volunteer/volunteer-email.controller.js
+++ client/app/volunteer/volunteer-email.controller.js
@@ -31,42 +31,40 @@ Thank you very much for volunteering as an Odyssey of the Mind judge. On tournam
 
 {{ assignments | assignmentList }}
 
-If there are any changes you need to make to this scheduele, please contact {{ contact_info }}
+If there are any changes you need to make to this schedule, please contact me.
 
 Sincerely,
 
-{{ organizer_name }}
-Odyssey of the Mind Organizer`,
+Your Odyssey of the Mind Organizer`,
       nonJudge: `Dear {{ volunteer.firstName }},
 
-Thank you very much for volunteering forshow
-
-If there are any issues with these job shifts please contact {{ contact_info }}
-
-Sincerely,
-
-{{ organizer_name }}
-Odyssey of the Mind Organizer`,
-      notNeeded: `Dear {{ volunteer.firstName }},
-
-Thank you very much for volunteering for Odyssey of the Mind. We have had an influx of volunteers this year and you are not needed as a volunteer. Again, we would like to thank you for volunteering and would hope that you volunteer again next year.
-
-Sincerely,
-
-{{ organizer_name }}
-Odyssey of the Mind Organizer`,
-      changes: `Dear {{ volunteer.firstName }},
-
-There have been changes made to the volunteer schedule for Odyssey of the Mind. These changes have affected you and your new schedule is:
+Thank you for your participation in this event! Your volunteer assigments are as follows:
 
 {{ assignments | assignmentList }}
 
-On tournament day, please report to the tournament location before your first shift. If there are any issues with these new job shifts please contact {{ contact_info }}
+If there are any issues with these job shifts, please contact me.
 
 Sincerely,
 
-{{ organizer_name }}
-Odyssey of the Mind Organizer`
+Your Odyssey of the Mind Organizer`,
+      notNeeded: `Dear {{ volunteer.firstName }},
+
+Thank you for your participation in this event! Due to a variety of constraints, we were unable to assign you to any volunteer positions, but we hope you still enjoy coming to watch the performance.
+
+Sincerely,
+
+Your Odyssey of the Mind Organizer`,
+      changes: `Dear {{ volunteer.firstName }},
+
+There have been changes made to the volunteer schedule for Odyssey of the Mind. Your new schedule is:
+
+{{ assignments | assignmentList }}
+
+On tournament day, please report to the tournament location before your first shift. If there are any issues with these new job shifts please contact me.
+
+Sincerely,
+
+Your Odyssey of the Mind Organizer`
     };
 
     $scope.cancel = function() {
@@ -90,11 +88,33 @@ Odyssey of the Mind Organizer`
     };
   })
   .filter('assignmentList', function() {
+    var parseTime = function(time) {
+      if(time) {
+        var strTime = "";
+        if(time >= 1300) {
+          time = time - 1200;
+          strTime = time.toString();
+          strTime = strTime.substring(0, strTime.length / 2) + ":" + strTime.substring(strTime.length / 2, strTime.length);
+          strTime = strTime + " PM";
+        } else if(time >= 1200) {
+          strTime = time.toString();
+          strTime = strTime.substring(0, strTime.length / 2) + ":" + strTime.substring(strTime.length / 2, strTime.length);
+          strTime = strTime + " PM";
+        } else {
+          strTime = time.toString();
+          strTime = strTime.substring(0, strTime.length / 2) + ":" + strTime.substring(strTime.length / 2, strTime.length);
+          strTime = strTime + " AM";
+        }
+
+        return strTime;
+      }
+    };
+
     return function(assignments) {
       var list = '';
 
       assignments.forEach(function(assignment) {
-        list += assignment.slot.start + '-' + assignment.slot.end + ': ' +
+        list += parseTime(assignment.slot.start) + '-' + parseTime(assignment.slot.end) + ': ' +
           assignment.job.title + ' ' +
           '(' + assignment.location.name + ')\n'
       });
